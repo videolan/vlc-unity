4b106861710c14ff4eb6fe6620d71296178eb945
Théo Jacquin <42nimag@gmail.com>
2017-07-10 14:48:53 +0200

    Improve demo, allow character to move
    To improve this demo code, it would be nice to show LibVLC integration
    with a true unity game base code
    
    This commit allows charactere to move and to rotate, it attaches a
    sample CharacterController to camara to act like a first-person
    character, which can be moved and rotated with arrow keys and ijkl
    keys

---
+++ Assets/CharacterControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour {
+
+     private Vector3 moveDirection = Vector3.zero;
+     private Vector3 rotationDirection = Vector3.zero;
+     private float moveSpeed = 5;
+     private float rotationSpeed = 15;
+
+
+     private void Update()
+	  {
+	       CharacterController controller = GetComponent<CharacterController>();
+
+	       moveDirection = transform.TransformDirection(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	       moveDirection *= moveSpeed;
+	       controller.Move(moveDirection * Time.deltaTime);
+
+	       rotationDirection = new Vector3(Input.GetAxisRaw("Vertical2"), Input.GetAxisRaw("Horizontal2"), 0);
+	       rotationDirection *= rotationSpeed * Time.deltaTime;
+	       transform.Rotate(rotationDirection);
+	  }
+}
