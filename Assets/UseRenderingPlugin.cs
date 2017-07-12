da9193c3e9a9e16455967a9351e998e0329fcf07
Théo Jacquin <42nimag@gmail.com>
2017-07-12 16:27:34 +0200

    CLEANUP Clean code to follow GNU coding style and add comment

--- Assets/UseRenderingPlugin.cs
+++ Assets/UseRenderingPlugin.cs
@@ -6,107 +6,111 @@ using System.Runtime.InteropServices;
 
 public class UseRenderingPlugin : MonoBehaviour
 {
-	// Native plugin rendering events are only called if a plugin is used
-	// by some script. This means we have to DllImport at least
-	// one function in some active script.
-	// For this example, we'll call into plugin's SetTimeFromUnity
-	// function and pass the current time so the plugin can animate.
+  // Native plugin rendering events are only called if a plugin is used
+  // by some script. This means we have to DllImport at least
+  // one function in some active script.
+  // For this example, we'll call into plugin's SetTimeFromUnity
+  // function and pass the current time so the plugin can animate.
 
 #if (UNITY_IPHONE || UNITY_WEBGL) && !UNITY_EDITOR
-	[DllImport ("__Internal")]
+  [DllImport ("__Internal")]
 #else
-	[DllImport ("VlcUnityWrapper")]
+  [DllImport ("VlcUnityWrapper")]
 #endif
-	private static extern void SetTimeFromUnity(float t);
+  private static extern void SetTimeFromUnity (float t);
 
-
-	// We'll also pass native pointer to a texture in Unity.
-	// The plugin will fill texture data from native code.
+  // We'll also pass native pointer to a texture in Unity.
+  // The plugin will fill texture data from native code.
 #if (UNITY_IPHONE || UNITY_WEBGL) && !UNITY_EDITOR
-	[DllImport ("__Internal")]
+  [DllImport ("__Internal")]
 #else
-	[DllImport ("VlcUnityWrapper")]
+  [DllImport ("VlcUnityWrapper")]
 #endif
-	private static extern void SetTextureFromUnity(System.IntPtr texture, int w, int h);
+  private static extern void SetTextureFromUnity (System.IntPtr texture, int w, int h);
 
 #if (UNITY_IPHONE || UNITY_WEBGL) && !UNITY_EDITOR
-	[DllImport ("__Internal")]
+  [DllImport ("__Internal")]
 #else
-	[DllImport("VlcUnityWrapper")]
+  [DllImport ("VlcUnityWrapper")]
 #endif
-	private static extern IntPtr GetRenderEventFunc();
-
+  private static extern IntPtr GetRenderEventFunc ();
   
 #if (UNITY_IPHONE || UNITY_WEBGL) && !UNITY_EDITOR
-	[DllImport ("__Internal")]
+  [DllImport ("__Internal")]
 #else
-	[DllImport("VlcUnityWrapper")]
+  [DllImport ("VlcUnityWrapper")]
 #endif
-	private static extern void launchVLC();
-
+  private static extern void launchVLC ();
   
 #if (UNITY_IPHONE || UNITY_WEBGL) && !UNITY_EDITOR
-	[DllImport ("__Internal")]
+  [DllImport ("__Internal")]
 #else
-	[DllImport("VlcUnityWrapper")]
+  [DllImport ("VlcUnityWrapper")]
 #endif
-	private static extern void stopVLC();
-
+  private static extern void stopVLC ();
   
 #if UNITY_WEBGL && !UNITY_EDITOR
-	[DllImport ("__Internal")]
-	private static extern void RegisterPlugin();
+  [DllImport ("__Internal")]
+  private static extern void RegisterPlugin ();
 #endif
 
-	IEnumerator Start()
-	{
-  //      StartCoroutine("launchingVLC");
+  IEnumerator
+  Start ()
+  {
 #if UNITY_WEBGL && !UNITY_EDITOR
-		RegisterPlugin();
+    RegisterPlugin();
 #endif
-		CreateTextureAndPassToPlugin();
-        launchVLC();
-		yield return StartCoroutine("CallPluginAtEndOfFrames");
-	}
 
-	private void CreateTextureAndPassToPlugin()
-	{
-        // Scale the texture as a screen 16/9 wide
-        transform.localScale = new Vector3(-1.0f, 1.0f, 0.5625f);
+    CreateTextureAndPassToPlugin ();
+    launchVLC ();
 
-		// Create a texture
-		Texture2D tex = new Texture2D(1920,1080,TextureFormat.BGRA32,false);
-		// Set point filtering just so we can see the pixels clearly
-		//tex.filterMode = FilterMode.Point;
-		// Call Apply() so it's actually uploaded to the GPU
-		tex.Apply();
+    yield return StartCoroutine ("CallPluginAtEndOfFrames");
+  }
 
-		// Set texture onto our material
-		GetComponent<Renderer>().material.mainTexture = tex;
+  private void
+  CreateTextureAndPassToPlugin ()
+  {
+    // Scale the texture as a screen 16/9 wide, so h = 9/16 ~= 0.5625
+    transform.localScale = new Vector3 (-1.0f, 1.0f, 0.5625f);
 
-		// Pass texture pointer to the plugin
-		SetTextureFromUnity (tex.GetNativeTexturePtr(), tex.width, tex.height);
-	}
+    // Create a texture. TextureFormat should match the one you
+    // choosed in liblvc_video_set_format.
+    // RV32 matches BGRA32 if you use libvlc <= v2 (there's a bug that inverts chroma)
+    // RV32 matches RGBA32 if you use libvlc >  v2
+    Texture2D tex = new Texture2D (1920, 1080, TextureFormat.BGRA32, false);
 
-	private IEnumerator CallPluginAtEndOfFrames()
-	{
-		while (true) {
-			// Wait until all frame rendering is done
-			yield return new WaitForEndOfFrame();
-          
-			// Set time for the plugin
-			SetTimeFromUnity (Time.timeSinceLevelLoad);
+    // Call Apply() so it's actually uploaded to the GPU
+    tex.Apply();
 
-			// Issue a plugin event with arbitrary integer identifier.
-			// The plugin can distinguish between different
-			// things it needs to do based on this ID.
-			// For our simple plugin, it does not matter which ID we pass here.
-			GL.IssuePluginEvent(GetRenderEventFunc(), 1);
+    // Set texture onto our material
+    GetComponent<Renderer>().material.mainTexture = tex;
 
-            if(Time.timeSinceLevelLoad > 40.0f) {
-                stopVLC();
-                break;
-            }
-		}
-	}
+    // Pass texture pointer to the plugin
+    SetTextureFromUnity (tex.GetNativeTexturePtr(), tex.width, tex.height);
+  }
+
+  private IEnumerator
+  CallPluginAtEndOfFrames()
+  {
+    while (true)
+      {
+	// Wait until all frame rendering is done
+	yield return new WaitForEndOfFrame();
+
+	// Set time for the plugin
+	SetTimeFromUnity (Time.timeSinceLevelLoad);
+
+	// Issue a plugin event with arbitrary integer identifier.
+	GL.IssuePluginEvent(GetRenderEventFunc(), 1);
+
+
+	// // Code to automatically stop after 40sec, uncomment if needed
+	// if(Time.timeSinceLevelLoad > 40.0f)
+	//   {
+	//     stopVLC();
+	//     break;
+	//   }
+
+      }
+  }
 }
--- VLC-Unity-Wrapper/src/RenderingPlugin.cpp
+++ VLC-Unity-Wrapper/src/RenderingPlugin.cpp
@@ -39,170 +39,173 @@ static int   g_TextureRowPitch = 0;
 static unsigned char* vlcVideoFramePtr = NULL;
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-extern "C" void * lockfct(void *data, void **p_pixels)
+/* lockfct
+ * Callback for LibVLC to lock data before decoding
+ */
+extern "C" void *
+lockfct (void *data, void **p_pixels)
 {
-	//    void* textureHandle = g_TextureHandle;
-	int width = g_TextureWidth;
-	int height = g_TextureHeight;
-    
+  // Lock the mutex to ensure data safeness
+  pthread_mutex_lock(&mutex);
 
-	//*p_pixels = s_CurrentAPI->BeginModifyTexture(textureHandle, width, height, &g_TextureRowPitch);
-	// Lock the mutex to ensure data safeness
-	pthread_mutex_lock(&mutex);
-		
-	free(vlcVideoFramePtr);
-	vlcVideoFramePtr = (unsigned char *) malloc (width * height * 4);
-	*p_pixels = vlcVideoFramePtr;
+  free(vlcVideoFramePtr);
+  vlcVideoFramePtr = (unsigned char *) malloc (g_TextureWidth * g_TextureHeight * 4);
+  *p_pixels = vlcVideoFramePtr;
 
-	//fprintf(stderr, "[CUSTOMVLC] LOCK FUNCTION Called   : Texture PTR: %p, DataPTR: %p\n", g_TextureHandle, *p_pixels);
+  //fprintf(stderr, "[CUSTOMVLC] LOCK FUNCTION Called   : Texture PTR: %p, DataPTR: %p\n", g_TextureHandle, *p_pixels);
 
-	return NULL;//&textureDataPtr;
+  return NULL;
 }
 
-// Unlock func
-extern "C" void unlockfct(void *data, void *id, void * const *p_pixels)
+/* unlockfct
+ * Callback for LibVLC to release datas
+ */
+extern "C" void
+unlockfct (void *data, void *id, void * const *p_pixels)
 {
-	// Release datas
-	pthread_mutex_unlock(&mutex);
-	/*    void* textureHandle = g_TextureHandle;
-	      int width = g_TextureWidth;
-	      int height = g_TextureHeight;
-	      int textureRowPitch = g_TextureRowPitch;
-
-	      fprintf(stderr, "[CUSTOMVLC] UNLOCK FUNCTION Called : Texture PTR: %p, DataPTR: %p\n", textureHandle, *p_pixels);
-
-	      unsigned char* dst = (unsigned char*) *p_pixels;
-	      for (int y = 0; y < height; ++y)
-	      {
-	      unsigned char* ptr = dst;
-	      for (int x = 0; x < width; ++x)
-	      {
-	      ptr[0] = (int) 0;
-	      ptr[1] = (int) y % 255;
-	      ptr[2] = (int) (x*10+y) % 255;
-	      ptr[3] = (int) (x+10*y) % 255;
-                    
-	      ptr += 4;
-	      }
-            
-	      dst += g_TextureRowPitch;
-	      }
-
-	      s_CurrentAPI->EndModifyTexture(textureHandle, width, height, textureRowPitch, *p_pixels);
-	*/
+  // Release datas
+  pthread_mutex_unlock(&mutex);
 }
 
 
-static void ModifyTexturePixels()
+static void
+ModifyTexturePixels ()
 {
-	void* textureHandle = g_TextureHandle;
-	int width = g_TextureWidth;
-	int height = g_TextureHeight;
-	if (!textureHandle)
-	    return;
+  void* textureHandle = g_TextureHandle;
+  int width = g_TextureWidth;
+  int height = g_TextureHeight;
+  if (!textureHandle)
+    return;
     
-	// Lock mutex to ensure all datas had been written
-	pthread_mutex_lock(&mutex);
+  // Lock mutex to ensure all datas had been written
+  pthread_mutex_lock(&mutex);
     
-	int textureRowPitch;
-	void* textureDataPtr = s_CurrentAPI->BeginModifyTexture(textureHandle, width, height, &textureRowPitch);
-	if (!textureDataPtr)
-	    return;
+  int textureRowPitch;
+  void* textureDataPtr = s_CurrentAPI->BeginModifyTexture(textureHandle, width, height, &textureRowPitch);
+  if (!textureDataPtr)
+    return;
     
-	//fprintf(stderr, "[CUSTOMVLC] MODIFY TEXTURE Called  : Texture PTR: %p, DataPTR: %p\n", textureHandle, textureDataPtr);
+  //fprintf(stderr, "[CUSTOMVLC] MODIFY TEXTURE Called  : Texture PTR: %p, DataPTR: %p\n", textureHandle, textureDataPtr);
 
-	unsigned char* ptr = (unsigned char*)textureDataPtr;
-	unsigned char* origin = (unsigned char*) vlcVideoFramePtr;
+  unsigned char* ptr = (unsigned char *) textureDataPtr;
+  unsigned char* origin = (unsigned char *) vlcVideoFramePtr;
     
-	for(int i = 0; i < height*width*4; ++i) {
-	    *ptr = *origin;
-	    ++ptr;
-	    ++origin;
-	}
+  for (int i = 0; i < height*width*4; ++i)
+    {
+      *ptr = *origin;
+      ++ptr;
+      ++origin;
+    }
 
-	s_CurrentAPI->EndModifyTexture(textureHandle, width, height, textureRowPitch, textureDataPtr);
+  s_CurrentAPI->EndModifyTexture(textureHandle, width, height, textureRowPitch, textureDataPtr);
 
-	// Release datas
-	pthread_mutex_unlock(&mutex);
+  // Release datas
+  pthread_mutex_unlock(&mutex);
 }
 
 
-extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API SetTextureFromUnity(void* textureHandle, int w, int h)
+extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
+SetTextureFromUnity (void* textureHandle, int w, int h)
 {
-	// A script calls this at initialization time; just remember the texture pointer here.
-	// Will update texture pixels each frame from the plugin rendering event (texture update
-	// needs to happen on the rendering thread).
-	g_TextureHandle = textureHandle;
-	g_TextureWidth = w;
-	g_TextureHeight = h;
+  // A script calls this at initialization time; just remember the texture pointer here.
+  // Will update texture pixels each frame from the plugin rendering event (texture update
+  // needs to happen on the rendering thread).
+  g_TextureHandle = textureHandle;
+  g_TextureWidth = w;
+  g_TextureHeight = h;
 }
 
 libvlc_instance_t * inst;
 libvlc_media_player_t *mp;
 libvlc_media_t *m;
 
-extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API launchVLC() {
-	//    fprintf(stderr, "[CUSTOMVLC]");
-	vlcVideoFramePtr = (unsigned char *) malloc (g_TextureWidth * g_TextureHeight * 4);
 
-	fprintf(stderr, "[CUSTOMVLC] Instantiating mutex...\n");
-	if (pthread_mutex_init(&mutex, NULL) != 0)
-	    fprintf(stderr, "[CUSTOMVLC] Mutex init failed\n");
-  
-	inst = libvlc_new (0, NULL);
-    
-	fprintf(stderr, "[CUSTOMVLC] Instantiating LibLVC...\n");
-    
-	// Create a new item 
-	m = libvlc_media_new_location (inst, VIDEOFILE);
-	mp = libvlc_media_player_new_from_media (m);
-    
+/** LibVLC's API function exported to Unity
+ *
+ * Every following functions will be exported to. Unity We have to
+ * redeclare the LibVLC's function for the keyword
+ * UNITY_INTERFACE_EXPORT and UNITY_INTERFACE_API
+ */
 
-	libvlc_media_release (m);
-	libvlc_release(inst);
+extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
+launchVLC () {
+  // Create a mutex, to share data between LibVLC's callback and Unity
+  fprintf (stderr, "[CUSTOMVLC] Instantiating mutex...\n");
+  if (pthread_mutex_init (&mutex, NULL) != 0)
+    fprintf(stderr, "[CUSTOMVLC] Mutex init failed\n");
 
-	libvlc_video_set_callbacks(mp, lockfct, unlockfct, NULL, NULL);
-	libvlc_video_set_format(mp, "RV32", g_TextureWidth, g_TextureHeight, g_TextureWidth*4);
+  // Create an instance of LibVLC
+  fprintf(stderr, "[CUSTOMVLC] Instantiating LibLVC...\n");
+  inst = libvlc_new (0, NULL);
 
-	libvlc_media_player_play (mp);
+  // Create a new item
+  m = libvlc_media_new_location (inst, VIDEOFILE);
+  mp = libvlc_media_player_new_from_media (m);
+
+  // Release the media and the player since we don't need them anymore
+  libvlc_media_release (m);
+  libvlc_release(inst);
+
+  // Instantiate a first buffer from frame, because our callbacks will
+  // try to free it
+  vlcVideoFramePtr = (unsigned char *) malloc (g_TextureWidth * g_TextureHeight * 4);
+
+  // Set callbacks for activating vmem. Vmem let us handle video output
+  // separatly from LibVLC classical way
+  libvlc_video_set_callbacks(mp, lockfct, unlockfct, NULL, NULL);
+  libvlc_video_set_format(mp, "RV32", g_TextureWidth, g_TextureHeight, g_TextureWidth*4);
+
+  // Play the media
+  libvlc_media_player_play (mp);
 }	
 
-extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API stopVLC() {
-	// Stop playing 
-	libvlc_media_player_stop (mp);
+extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
+stopVLC () {
+  // Stop playing
+  libvlc_media_player_stop (mp);
  
-	// Free the media_player
-	libvlc_media_player_release (mp);
-	//libvlc_release (inst);
+  // Free the media_player
+  libvlc_media_player_release (mp);
 
-	fprintf(stderr, "[CUSTOMVLC] VLC STOPPED\n");
+  fprintf(stderr, "[CUSTOMVLC] VLC STOPPED\n");
 }
 
-extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API playPauseVLC() {
-	// Stop playing 
-	libvlc_media_player_pause (mp);
+extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
+playPauseVLC () {
+  // Pause playing
+  libvlc_media_player_pause (mp);
  
-	fprintf(stderr, "[CUSTOMVLC] VLC PAUSE TOGGLED\n");
+  fprintf(stderr, "[CUSTOMVLC] VLC PAUSE TOGGLED\n");
 }
 
-extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API pauseVLC() {
-	// Stop playing 
-	libvlc_media_player_pause (mp);
+extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
+pauseVLC () {
+  // Paused playing
+  libvlc_media_player_pause (mp);
  
-	fprintf(stderr, "[CUSTOMVLC] VLC PAUSED\n");
+  fprintf(stderr, "[CUSTOMVLC] VLC PAUSED\n");
 }
 
-extern "C" float UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API getPositionVLC() {
-	return libvlc_media_player_get_position (mp);
+extern "C" float UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
+getPositionVLC () {
+  return libvlc_media_player_get_position (mp);
 }
 
-extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API setPositionVLC(float pos) {
-	if (pos <= 1.0 && pos >= 0.0)
-	    libvlc_media_player_set_position (mp, pos);
+extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
+setPositionVLC (float pos) {
+  if (pos <= 1.0 && pos >= 0.0)
+    libvlc_media_player_set_position (mp, pos);
 }
+
+
+/** Unity API function
+ *
+ * Following functions are needed for integration into Unity's API.
+ * UnitySetInterfaces
+ */
 
 // --------------------------------------------------------------------------
-// UnitySetInterfaces
+//  UnitySetInterfaces
 
 static void UNITY_INTERFACE_API OnGraphicsDeviceEvent(UnityGfxDeviceEventType eventType);
 
@@ -211,17 +214,17 @@ static IUnityGraphics* s_Graphics = NULL;
 
 extern "C" void	UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginLoad(IUnityInterfaces* unityInterfaces)
 {
-	s_UnityInterfaces = unityInterfaces;
-	s_Graphics = s_UnityInterfaces->Get<IUnityGraphics>();
-	s_Graphics->RegisterDeviceEventCallback(OnGraphicsDeviceEvent);
+  s_UnityInterfaces = unityInterfaces;
+  s_Graphics = s_UnityInterfaces->Get<IUnityGraphics>();
+  s_Graphics->RegisterDeviceEventCallback(OnGraphicsDeviceEvent);
 	
-	// Run OnGraphicsDeviceEvent(initialize) manually on plugin load
-	OnGraphicsDeviceEvent(kUnityGfxDeviceEventInitialize);
+  // Run OnGraphicsDeviceEvent(initialize) manually on plugin load
+  OnGraphicsDeviceEvent(kUnityGfxDeviceEventInitialize);
 }
 
 extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginUnload()
 {
-	s_Graphics->UnregisterDeviceEventCallback(OnGraphicsDeviceEvent);
+  s_Graphics->UnregisterDeviceEventCallback(OnGraphicsDeviceEvent);
 }
 
 #if UNITY_WEBGL
@@ -232,7 +235,7 @@ extern "C" void	UnityRegisterRenderingPlugin(PluginLoadFunc loadPlugin, PluginUn
 
 extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API RegisterPlugin()
 {
-	UnityRegisterRenderingPlugin(UnityPluginLoad, UnityPluginUnload);
+  UnityRegisterRenderingPlugin(UnityPluginLoad, UnityPluginUnload);
 }
 #endif
 
@@ -242,27 +245,27 @@ extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API RegisterPlugin()
 
 static void UNITY_INTERFACE_API OnGraphicsDeviceEvent(UnityGfxDeviceEventType eventType)
 {
-	// Create graphics API implementation upon initialization
-	if (eventType == kUnityGfxDeviceEventInitialize)
-	    {
-		assert(s_CurrentAPI == NULL);
-		s_DeviceType = s_Graphics->GetRenderer();
-		s_CurrentAPI = CreateRenderAPI(s_DeviceType);
-	    }
+  // Create graphics API implementation upon initialization
+  if (eventType == kUnityGfxDeviceEventInitialize)
+    {
+      assert(s_CurrentAPI == NULL);
+      s_DeviceType = s_Graphics->GetRenderer();
+      s_CurrentAPI = CreateRenderAPI(s_DeviceType);
+    }
 
-	// Let the implementation process the device related events
-	if (s_CurrentAPI)
-	    {
-		s_CurrentAPI->ProcessDeviceEvent(eventType, s_UnityInterfaces);
-	    }
+  // Let the implementation process the device related events
+  if (s_CurrentAPI)
+    {
+      s_CurrentAPI->ProcessDeviceEvent(eventType, s_UnityInterfaces);
+    }
 
-	// Cleanup graphics API implementation upon shutdown
-	if (eventType == kUnityGfxDeviceEventShutdown)
-	    {
-		delete s_CurrentAPI;
-		s_CurrentAPI = NULL;
-		s_DeviceType = kUnityGfxRendererNull;
-	    }
+  // Cleanup graphics API implementation upon shutdown
+  if (eventType == kUnityGfxDeviceEventShutdown)
+    {
+      delete s_CurrentAPI;
+      s_CurrentAPI = NULL;
+      s_DeviceType = kUnityGfxRendererNull;
+    }
 }
 
 
@@ -274,11 +277,11 @@ static void UNITY_INTERFACE_API OnGraphicsDeviceEvent(UnityGfxDeviceEventType ev
 
 static void UNITY_INTERFACE_API OnRenderEvent(int eventID)
 {
-	// Unknown / unsupported graphics device type? Do nothing
-	if (s_CurrentAPI == NULL)
-	    return;
+  // Unknown / unsupported graphics device type? Do nothing
+  if (s_CurrentAPI == NULL)
+    return;
 
-	ModifyTexturePixels();
+  ModifyTexturePixels();
 }
 
 
@@ -287,6 +290,6 @@ static void UNITY_INTERFACE_API OnRenderEvent(int eventID)
 
 extern "C" UnityRenderingEvent UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API GetRenderEventFunc()
 {
-	return OnRenderEvent;
+  return OnRenderEvent;
 }
 
