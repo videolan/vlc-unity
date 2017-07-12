6d9b90a06913105c28042e49f488feea78f9f5ba
Théo Jacquin <42nimag@gmail.com>
2017-07-12 09:43:28 +0200

    Rename libvlc-wrapper folder
    Because it was simpler for auto-completion purposes, as there's
    already a Lib folder

--- VLC-Unity-Wrapper/src/RenderingPlugin.cpp
+++ VLC-Unity-Wrapper/src/RenderingPlugin.cpp
@@ -39,90 +39,91 @@ static int   g_TextureRowPitch = 0;
 static unsigned char* vlcVideoFramePtr = NULL;
 static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-extern "C" void * lockfct(void *data, void **p_pixels) {
-    //    void* textureHandle = g_TextureHandle;
-    int width = g_TextureWidth;
-    int height = g_TextureHeight;
+extern "C" void * lockfct(void *data, void **p_pixels)
+{
+	//    void* textureHandle = g_TextureHandle;
+	int width = g_TextureWidth;
+	int height = g_TextureHeight;
     
 
-    //*p_pixels = s_CurrentAPI->BeginModifyTexture(textureHandle, width, height, &g_TextureRowPitch);
-    // Lock the mutex to ensure data safeness
-    pthread_mutex_lock(&mutex);
-
-    free(vlcVideoFramePtr);
+	//*p_pixels = s_CurrentAPI->BeginModifyTexture(textureHandle, width, height, &g_TextureRowPitch);
+	// Lock the mutex to ensure data safeness
+	pthread_mutex_lock(&mutex);
+		
+	free(vlcVideoFramePtr);
 	vlcVideoFramePtr = (unsigned char *) malloc (width * height * 4);
-    *p_pixels = vlcVideoFramePtr;
+	*p_pixels = vlcVideoFramePtr;
 
-    fprintf(stderr, "[CUSTOMVLC] LOCK FUNCTION Called   : Texture PTR: %p, DataPTR: %p\n", g_TextureHandle, *p_pixels);
+	//fprintf(stderr, "[CUSTOMVLC] LOCK FUNCTION Called   : Texture PTR: %p, DataPTR: %p\n", g_TextureHandle, *p_pixels);
 
-    return NULL;//&textureDataPtr;
+	return NULL;//&textureDataPtr;
 }
 
 // Unlock func
-extern "C" void unlockfct(void *data, void *id, void * const *p_pixels) {
-  // Release datas
-  pthread_mutex_unlock(&mutex);
-    /*    void* textureHandle = g_TextureHandle;
-    int width = g_TextureWidth;
-    int height = g_TextureHeight;
-    int textureRowPitch = g_TextureRowPitch;
+extern "C" void unlockfct(void *data, void *id, void * const *p_pixels)
+{
+	// Release datas
+	pthread_mutex_unlock(&mutex);
+	/*    void* textureHandle = g_TextureHandle;
+	      int width = g_TextureWidth;
+	      int height = g_TextureHeight;
+	      int textureRowPitch = g_TextureRowPitch;
 
-    fprintf(stderr, "[CUSTOMVLC] UNLOCK FUNCTION Called : Texture PTR: %p, DataPTR: %p\n", textureHandle, *p_pixels);
+	      fprintf(stderr, "[CUSTOMVLC] UNLOCK FUNCTION Called : Texture PTR: %p, DataPTR: %p\n", textureHandle, *p_pixels);
 
-    unsigned char* dst = (unsigned char*) *p_pixels;
-	for (int y = 0; y < height; ++y)
-        {
-            unsigned char* ptr = dst;
-            for (int x = 0; x < width; ++x)
-                {
-                    ptr[0] = (int) 0;
-                    ptr[1] = (int) y % 255;
-                    ptr[2] = (int) (x*10+y) % 255;
-                    ptr[3] = (int) (x+10*y) % 255;
+	      unsigned char* dst = (unsigned char*) *p_pixels;
+	      for (int y = 0; y < height; ++y)
+	      {
+	      unsigned char* ptr = dst;
+	      for (int x = 0; x < width; ++x)
+	      {
+	      ptr[0] = (int) 0;
+	      ptr[1] = (int) y % 255;
+	      ptr[2] = (int) (x*10+y) % 255;
+	      ptr[3] = (int) (x+10*y) % 255;
                     
-                    ptr += 4;
-                }
+	      ptr += 4;
+	      }
             
-            dst += g_TextureRowPitch;
-        }
+	      dst += g_TextureRowPitch;
+	      }
 
-    s_CurrentAPI->EndModifyTexture(textureHandle, width, height, textureRowPitch, *p_pixels);
-    */
+	      s_CurrentAPI->EndModifyTexture(textureHandle, width, height, textureRowPitch, *p_pixels);
+	*/
 }
 
 
 static void ModifyTexturePixels()
 {
-    void* textureHandle = g_TextureHandle;
-    int width = g_TextureWidth;
-    int height = g_TextureHeight;
-   	if (!textureHandle)
-		return;
-
-    // Lock mutex to ensure all datas had been written
-    pthread_mutex_lock(&mutex);
-
+	void* textureHandle = g_TextureHandle;
+	int width = g_TextureWidth;
+	int height = g_TextureHeight;
+	if (!textureHandle)
+	    return;
+    
+	// Lock mutex to ensure all datas had been written
+	pthread_mutex_lock(&mutex);
+    
 	int textureRowPitch;
 	void* textureDataPtr = s_CurrentAPI->BeginModifyTexture(textureHandle, width, height, &textureRowPitch);
 	if (!textureDataPtr)
-		return;
+	    return;
+    
+	//fprintf(stderr, "[CUSTOMVLC] MODIFY TEXTURE Called  : Texture PTR: %p, DataPTR: %p\n", textureHandle, textureDataPtr);
 
-    fprintf(stderr, "[CUSTOMVLC] MODIFY TEXTURE Called  : Texture PTR: %p, DataPTR: %p\n", textureHandle, textureDataPtr);
-
-    unsigned char* ptr = (unsigned char*)textureDataPtr;
-    unsigned char* origin = (unsigned char*) vlcVideoFramePtr;
-
-    for(int i = 0; i < height*width*4; ++i)
-        {
-            *ptr = *origin;
-            ++ptr;
-            ++origin;
-        }
+	unsigned char* ptr = (unsigned char*)textureDataPtr;
+	unsigned char* origin = (unsigned char*) vlcVideoFramePtr;
+    
+	for(int i = 0; i < height*width*4; ++i) {
+	    *ptr = *origin;
+	    ++ptr;
+	    ++origin;
+	}
 
 	s_CurrentAPI->EndModifyTexture(textureHandle, width, height, textureRowPitch, textureDataPtr);
 
-    // Release datas
-    pthread_mutex_unlock(&mutex);
+	// Release datas
+	pthread_mutex_unlock(&mutex);
 }
 
 
@@ -141,15 +142,16 @@ libvlc_media_player_t *mp;
 libvlc_media_t *m;
 
 extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API launchVLC() {
-    //    fprintf(stderr, "[CUSTOMVLC]");
+	//    fprintf(stderr, "[CUSTOMVLC]");
 	vlcVideoFramePtr = (unsigned char *) malloc (g_TextureWidth * g_TextureHeight * 4);
 
-    inst = libvlc_new (0, NULL);
-  if (pthread_mutex_init(&mutex, NULL) != 0)
-    fprintf(stderr, "[CUSTOMVLC] Mutex init failed\n");
+	fprintf(stderr, "[CUSTOMVLC] Instantiating mutex...\n");
+	if (pthread_mutex_init(&mutex, NULL) != 0)
+	    fprintf(stderr, "[CUSTOMVLC] Mutex init failed\n");
   
+	inst = libvlc_new (0, NULL);
     
-    fprintf(stderr, "[CUSTOMVLC] Instantiating LibLVC...\n");
+	fprintf(stderr, "[CUSTOMVLC] Instantiating LibLVC...\n");
     
 	// Create a new item 
 	m = libvlc_media_new_location (inst, VIDEOFILE);
@@ -157,23 +159,23 @@ extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API launchVLC() {
     
 
 	libvlc_media_release (m);
-    libvlc_release(inst);
+	libvlc_release(inst);
 
-    libvlc_video_set_callbacks(mp, lockfct, unlockfct, NULL, NULL);
+	libvlc_video_set_callbacks(mp, lockfct, unlockfct, NULL, NULL);
 	libvlc_video_set_format(mp, "RV32", g_TextureWidth, g_TextureHeight, g_TextureWidth*4);
 
 	libvlc_media_player_play (mp);
 }	
 
 extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API stopVLC() {
-    // Stop playing 
+	// Stop playing 
 	libvlc_media_player_stop (mp);
  
 	// Free the media_player
 	libvlc_media_player_release (mp);
 	//libvlc_release (inst);
 
-    fprintf(stderr, "[CUSTOMVLC] VLC STOPPED\n");
+	fprintf(stderr, "[CUSTOMVLC] VLC STOPPED\n");
 }
 
 extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API playPauseVLC() {
@@ -198,7 +200,7 @@ extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API setPositionVLC(float 
 	if (pos <= 1.0 && pos >= 0.0)
 	    libvlc_media_player_set_position (mp, pos);
 }
-
+q
 // --------------------------------------------------------------------------
 // UnitySetInterfaces
 
@@ -242,25 +244,25 @@ static void UNITY_INTERFACE_API OnGraphicsDeviceEvent(UnityGfxDeviceEventType ev
 {
 	// Create graphics API implementation upon initialization
 	if (eventType == kUnityGfxDeviceEventInitialize)
-	{
+	    {
 		assert(s_CurrentAPI == NULL);
 		s_DeviceType = s_Graphics->GetRenderer();
 		s_CurrentAPI = CreateRenderAPI(s_DeviceType);
-	}
+	    }
 
 	// Let the implementation process the device related events
 	if (s_CurrentAPI)
-	{
+	    {
 		s_CurrentAPI->ProcessDeviceEvent(eventType, s_UnityInterfaces);
-	}
+	    }
 
 	// Cleanup graphics API implementation upon shutdown
 	if (eventType == kUnityGfxDeviceEventShutdown)
-	{
+	    {
 		delete s_CurrentAPI;
 		s_CurrentAPI = NULL;
 		s_DeviceType = kUnityGfxRendererNull;
-	}
+	    }
 }
 
 
@@ -274,7 +276,7 @@ static void UNITY_INTERFACE_API OnRenderEvent(int eventID)
 {
 	// Unknown / unsupported graphics device type? Do nothing
 	if (s_CurrentAPI == NULL)
-		return;
+	    return;
 
 	ModifyTexturePixels();
 }
