95d887ec3e45145541451bbac375b898c4f5d2b6
Keijiro Takahashi <keijiro@gmail.com>
2017-10-23 13:25:07 +0900

    MacOS support

--- Plugin/Common.mk
+++ Plugin/Common.mk
@@ -1,4 +1,4 @@
-TARGET=TestPlugin
+TARGET = TestPlugin
 
 SRCS = Test.cpp
 OBJS = $(SRCS:.cpp=.o)
@@ -6,17 +6,21 @@ OBJS = $(SRCS:.cpp=.o)
 CXXFLAGS = -O2 -Wall -std=c++11
 LDFLAGS = -shared
 
-ifeq ($(PLATFORM),Windows)
-    BIN_PREFIX=x86_64-w64-mingw32
+ifeq ($(PLATFORM), Windows)
+    BIN_PREFIX = x86_64-w64-mingw32
     OUTPUT = $(TARGET).dll
 else
     BIN_PREFIX =
-    OUTPUT = lib$(TARGET).so
     CXXFLAGS += -fPIC
     LDFLAGS += -rdynamic -fPIC
+    ifeq ($(PLATFORM), MacOS)
+        OUTPUT = $(TARGET).bundle
+    else
+        OUTPUT = lib$(TARGET).so
+    endif
 endif
 
-CXX = $(BIN_PREFIX)-g++
+CXX = $(BIN_PREFIX)-c++
 STRIP = $(BIN_PREFIX)-strip
 
 all: $(OUTPUT)
