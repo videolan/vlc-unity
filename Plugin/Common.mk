00aaeec7ded7ede66d4e77ef505756413c93361f
Martin Finkel <finkel2804@gmail.com>
2019-08-20 11:02:38 +0200

    update makefile

--- Plugin/Common.mk
+++ Plugin/Common.mk
@@ -1,10 +1,13 @@
-TARGET = TestPlugin
+TARGET = RenderingPlugin
 
-SRCS = Test.cpp
+# SRCS := $(wildcard *.cpp)
+SRCS = RenderingPlugin.cpp RenderAPI.cpp RenderAPI_D3D11.cpp Log.cpp
+
 OBJS = $(SRCS:.cpp=.o)
 
 CXXFLAGS = -O2 -Wall -std=c++11 -I./include/
 LDFLAGS = -shared
+LIBS = -L./vlc-4.0.0-dev/sdk/lib -lvlc 
 
 BIN_PREFIX = x86_64-w64-mingw32
 OUTPUT = $(TARGET).dll
@@ -24,7 +27,7 @@ clean:
 	rm -f $(OUTPUT) $(OBJS)
 
 $(OUTPUT): $(OBJS)
-	$(CXX) $(LDFLAGS) -o $(OUTPUT) $(OBJS)
+	$(CXX) $(LDFLAGS) -o $(OUTPUT) $(OBJS) $(LIBS)
 
 .cpp.o:
 	$(CXX) $(CXXFLAGS) -c -o $@ $<
