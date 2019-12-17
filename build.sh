26e9ea2fbaae9991ca0e7ee02efee6cae0159e0d
Martin Finkel <martin@videolabs.io>
2019-12-17 10:39:23 +0700

    Build: Adjust paths to new directory orders

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-cd Plugin && make clean && make
-cp RenderingPlugin.{dll,pdb} ../Assets/Plugins/x86_64 -f
+cd Assets/VLC-Unity-Windows/Plugins/Source && make clean && make
+cp RenderingPlugin.{dll,pdb} ../x86_64 -f
