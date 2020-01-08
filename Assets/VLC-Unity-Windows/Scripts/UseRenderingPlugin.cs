cd08c48bc4baeded759c9a2cd8ba7fc60b9f5769
Martin Finkel <martin@videolabs.io>
2020-01-08 13:39:48 +0700

    remove time display, simplify plugin script

--- Assets/VLC-Unity-Windows/Scripts/UseRenderingPlugin.cs
+++ Assets/VLC-Unity-Windows/Scripts/UseRenderingPlugin.cs
@@ -2,104 +2,47 @@ using UnityEngine;
 using System;
 using System.Collections;
 using LibVLCSharp.Shared;
-using System.Runtime.InteropServices;
 
 public class UseRenderingPlugin : MonoBehaviour
 {
-#if (UNITY_IPHONE || UNITY_WEBGL) && !UNITY_EDITOR
-    private const string dllname = "__Internal";
-#else
-    private const string dllname = "VlcUnityWrapper";
-#endif
-
-    private LibVLC LibVLC { get; set; }
-    private MediaPlayer MediaPlayer { get; set; }
-
-    // Native plugin rendering events are only called if a plugin is used
-    // by some script. This means we have to DllImport at least
-    // one function in some active script.
-    // For this example, we'll call into plugin's SetTimeFromUnity
-    // function and pass the current time so the plugin can animate.
-
-    // Menu to select video
-    public GameObject menuVideoSelector;
-    public RemoteTimeDisplayer rtd;
-
-    public int seekTimeDelta = 2000;
-
-    private Texture2D tex = null;
+    LibVLC LibVLC { get; set; }
+    MediaPlayer MediaPlayer { get; set; }
+    int seekTimeDelta = 2000;
+    Texture2D tex = null;
 
     void Awake()
     {
-        SetPluginPath(Application.dataPath);
-        InitLibVLC(); // slow
+        Core.Initialize(Application.dataPath);
+
+        LibVLC = new LibVLC("--verbose=2");
+
+        MediaPlayer = new MediaPlayer(LibVLC);
+
+        MediaPlayer.Play(new Media(LibVLC, "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4", FromType.FromLocation));
+
+        StartCoroutine("CallPluginAtEndOfFrames");
     }
 
-    void InitLibVLC()
+    public void seekForward()
     {
-        try
-        {
-            Core.Initialize($"{Application.dataPath}\\Plugins");
-            LibVLC = new LibVLC("--verbose=2");
-        }
-        catch (Exception ex)
-        {
-            Debug.Log(ex);
-        }
+        Debug.Log("[VLC] Seeking forward !");
+        MediaPlayer.Time += seekTimeDelta;
     }
 
-    void OnDisable() {
+    public void seekBackward()
+    {
+        Debug.Log("[VLC] Seeking backward !");
+        MediaPlayer.Time -= seekTimeDelta;
+    }
+
+    void OnDisable() 
+    {
         MediaPlayer?.Stop();
         MediaPlayer?.Dispose();
         MediaPlayer = null;
 
         LibVLC?.Dispose();
         LibVLC = null;
-    }
-
-    [DllImport("RenderingPlugin")]
-    static extern void Print(string toPrint);
-
-    [DllImport("RenderingPlugin")]
-    static extern void SetPluginPath(string path);
-
-    public void OnMenuClick (int index)
-    {
-        string movieURL;
-        
-        switch (index) {
-        case 1:
-            string text = UniClipboard.GetText ();
-            Uri uri = new Uri (text);
-            if ( uri.IsWellFormedOriginalString() )
-                movieURL = text;
-            else
-                return;
-            break;
-        case 2:
-            movieURL = Constants.Movie480p;
-            break;
-        case 3:
-            movieURL = Constants.Movie1080p;
-            break;
-        case 4:
-        default:
-            movieURL = Constants.Movie2160p;
-            break;
-        }
-
-        menuVideoSelector.SetActive (false);
-
-        if (MediaPlayer == null)
-        {
-            MediaPlayer = new MediaPlayer(LibVLC);
-        }
-
-        var r = MediaPlayer.Play(new Media(LibVLC, "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4", FromType.FromLocation));
-        //   MediaPlayer.Play(new Media(LibVLC, movieURL, FromType.FromLocation));
-
-        Debug.Log(r ? "Play successful" : "Play NOT successful");
-        StartCoroutine("CallPluginAtEndOfFrames");
     }
 
     public void playPause()
@@ -114,31 +57,14 @@ public class UseRenderingPlugin : MonoBehaviour
     public void stop ()
     {
         Debug.Log ("[VLC] Stopping Player !");
-        rtd.setPlaying (false);
         StopCoroutine ("CallPluginAtEndOfFrames");
         MediaPlayer?.Stop();
         tex = null;
         GetComponent<Renderer> ().material.mainTexture = null;
-        menuVideoSelector.SetActive (true);
-    }
-
-    public void seekForward()
-    {
-        Debug.Log ("[VLC] Seeking forward !");
-        MediaPlayer.Time += seekTimeDelta;
-    }
-
-    public void seekBackward()
-    {
-        Debug.Log ("[VLC] Seeking backward !");
-        MediaPlayer.Time -= seekTimeDelta;
     }
 
     void Start()
     {
-#if UNITY_WEBGL && !UNITY_EDITOR
-        RegisterPlugin();
-#endif
         Vector3 scale = transform.localScale;
         scale.x = -scale.x;
         transform.localScale = scale;
@@ -159,7 +85,7 @@ public class UseRenderingPlugin : MonoBehaviour
                 uint i_videoWidth = 0;
 
                 MediaPlayer.Size(0, ref i_videoWidth, ref i_videoHeight);
-                IntPtr texptr = MediaPlayer.GetFrame(out bool updated);
+                IntPtr texptr = MediaPlayer.GetTexture(out bool updated);
                 if (i_videoWidth != 0 && i_videoHeight != 0 && updated && texptr != IntPtr.Zero)
                 {
                     Debug.Log("Creating texture with height " + i_videoHeight + " and width " + i_videoWidth);
@@ -176,7 +102,7 @@ public class UseRenderingPlugin : MonoBehaviour
             }
             else if (tex != null)
             {
-                IntPtr texptr = MediaPlayer.GetFrame(out bool updated);
+                IntPtr texptr = MediaPlayer.GetTexture(out bool updated);
                 if (updated)
                 {
                     tex.UpdateExternalTexture(texptr);
