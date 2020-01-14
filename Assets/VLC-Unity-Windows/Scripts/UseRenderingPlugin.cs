ac34db74b75e846ebbe2cdbbcab5bdd16a51d5dd
Martin Finkel <martin@videolabs.io>
2020-01-14 16:30:51 +0700

    Windows sample: disable OSD
    https://code.videolan.org/mfkl/vlc-unity/issues/26

--- Assets/VLC-Unity-Windows/Scripts/UseRenderingPlugin.cs
+++ Assets/VLC-Unity-Windows/Scripts/UseRenderingPlugin.cs
@@ -14,7 +14,7 @@ public class UseRenderingPlugin : MonoBehaviour
     {
         Core.Initialize(Application.dataPath);
 
-        _libVLC = new LibVLC("--verbose=2");
+        _libVLC = new LibVLC("--no-osd","--verbose=2");
 
         _mediaPlayer = new MediaPlayer(_libVLC);
 
