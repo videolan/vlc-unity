0135cc8f0accbc229b6d8f54b85552068bb61f3c
Martin Finkel <martin@videolabs.io>
2020-02-06 10:23:19 +0700

    script: Simply texture update mechanism
    We can use the Update() function from MonoBehaviour instead of our own custom coroutine

--- Assets/VLC-Unity-Windows/Scripts/UseRenderingPlugin.cs
+++ Assets/VLC-Unity-Windows/Scripts/UseRenderingPlugin.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using System;
-using System.Collections;
 using LibVLCSharp.Shared;
 
 public class UseRenderingPlugin : MonoBehaviour
@@ -18,8 +17,6 @@ public class UseRenderingPlugin : MonoBehaviour
         _libVLC = new LibVLC("--no-osd","--verbose=2");
 
         _mediaPlayer = new MediaPlayer(_libVLC);
-
-        StartCoroutine("CallPluginAtEndOfFrames");
 
         PlayPause();
     }
@@ -57,6 +54,7 @@ public class UseRenderingPlugin : MonoBehaviour
         else
         {
             playing = true;
+
             if(_mediaPlayer.Media == null)
             {
                 _mediaPlayer.Media = new Media(_libVLC, "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4", FromType.FromLocation);
@@ -73,47 +71,39 @@ public class UseRenderingPlugin : MonoBehaviour
         playing = false;
         _mediaPlayer?.Stop();
         GetComponent<Renderer>().material.mainTexture = null;
+        tex = null;
     }
 
-    void Start()
+    void Update()
     {
-    }
+        if(!playing) return;
 
-    private IEnumerator CallPluginAtEndOfFrames()
-    {
-        while (true)
+        if (tex == null)
         {
-            // Wait until all frame rendering is done
-            yield return new WaitForEndOfFrame();
+            // If received size is not null, it and scale the texture
+            uint i_videoHeight = 0;
+            uint i_videoWidth = 0;
 
-            // We may not receive video size the first time           
-            if (GetComponent<Renderer>().material.mainTexture == null && playing)
+            _mediaPlayer.Size(0, ref i_videoWidth, ref i_videoHeight);
+            var texptr = _mediaPlayer.GetTexture(out bool updated);
+            if (i_videoWidth != 0 && i_videoHeight != 0 && updated && texptr != IntPtr.Zero)
             {
-                // If received size is not null, it and scale the texture
-                uint i_videoHeight = 0;
-                uint i_videoWidth = 0;
-
-                _mediaPlayer.Size(0, ref i_videoWidth, ref i_videoHeight);
-                IntPtr texptr = _mediaPlayer.GetTexture(out bool updated);
-                if (i_videoWidth != 0 && i_videoHeight != 0 && updated && texptr != IntPtr.Zero)
-                {
-                    Debug.Log("Creating texture with height " + i_videoHeight + " and width " + i_videoWidth);
-                    tex = Texture2D.CreateExternalTexture((int)i_videoWidth,
-                        (int)i_videoHeight,
-                        TextureFormat.RGBA32,
-                        false,
-                        true,
-                        texptr);
-                    GetComponent<Renderer>().material.mainTexture = tex;
-                }
+                Debug.Log("Creating texture with height " + i_videoHeight + " and width " + i_videoWidth);
+                tex = Texture2D.CreateExternalTexture((int)i_videoWidth,
+                    (int)i_videoHeight,
+                    TextureFormat.RGBA32,
+                    false,
+                    true,
+                    texptr);
+                GetComponent<Renderer>().material.mainTexture = tex;
             }
-            else if (tex != null && playing)
+        }
+        else if (tex != null)
+        {
+            var texptr = _mediaPlayer.GetTexture(out bool updated);
+            if (updated)
             {
-                IntPtr texptr = _mediaPlayer.GetTexture(out bool updated);
-                if (updated)
-                {
-                    tex.UpdateExternalTexture(texptr);
-                }
+                tex.UpdateExternalTexture(texptr);
             }
         }
     }
