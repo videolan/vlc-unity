82e92e55dfc004a0def4d3ce97c76b1963fe4a6f
Martin Finkel <me@martinfinkel.com>
2021-12-08 13:49:17 +0000

    use a double buffer to read/write from VLC/to Unity

--- PluginSource/RenderAPI_D3D11.cpp
+++ PluginSource/RenderAPI_D3D11.cpp
@@ -52,7 +52,8 @@ public:
     bool Setup(const libvlc_video_setup_device_cfg_t *cfg, libvlc_video_setup_device_info_t *out );
     void Resize(void (*report_size_change)(void *report_opaque, unsigned width, unsigned height), void *report_opaque );
 
-    ReadWriteTexture        *read_write;
+    ReadWriteTexture        *read_write[2];
+    bool                    write_on_first = false;
 
 private:
     void CreateResources();
@@ -114,7 +115,8 @@ bool Setup_cb( void **opaque, const libvlc_video_setup_device_cfg_t *cfg, libvlc
 void Cleanup_cb( void *opaque )
 {
     RenderAPI_D3D11 *me = reinterpret_cast<RenderAPI_D3D11*>(opaque);
-    me->read_write->Cleanup();
+    me->read_write[0]->Cleanup();
+    me->read_write[1]->Cleanup();
 }
 
 void Resize_cb( void *opaque,
@@ -132,12 +134,14 @@ RenderAPI* CreateRenderAPI_D3D11()
 
 RenderAPI_D3D11::RenderAPI_D3D11()
 {
-    read_write = new ReadWriteTexture();
+    read_write[0] = new ReadWriteTexture();
+    read_write[1] = new ReadWriteTexture();
 }
 
 RenderAPI_D3D11::~RenderAPI_D3D11()
 {
-    delete read_write;
+    delete read_write[0];
+    delete read_write[1];
 }
 
 void RenderAPI_D3D11::setVlcContext(libvlc_media_player_t *mp)
@@ -208,7 +212,8 @@ void RenderAPI_D3D11::Update(UINT width, UINT height)
 
     m_width = width;
     m_height = height;
-    read_write->Update(m_width, m_height, m_d3deviceUnity, m_d3deviceVLC);
+    read_write[0]->Update(m_width, m_height, m_d3deviceUnity, m_d3deviceVLC);
+    read_write[1]->Update(m_width, m_height, m_d3deviceUnity, m_d3deviceVLC);
 
     LeaveCriticalSection(&m_outputLock);
 }
@@ -435,6 +440,7 @@ void RenderAPI_D3D11::Swap()
 {
     EnterCriticalSection(&m_outputLock);
     m_updated = true;
+    write_on_first = !write_on_first;
     LeaveCriticalSection(&m_outputLock);
 }
 
@@ -447,9 +453,14 @@ bool RenderAPI_D3D11::SelectPlane( size_t plane, void *output )
 {
     if ( plane != 0 || m_d3dctxVLC == NULL ) // we only support one packed RGBA plane (DXGI_FORMAT_R8G8B8A8_UNORM)
         return false;
+    
+    size_t write_index = write_on_first ? 0 : 1;
+
     static const FLOAT blackRGBA[4] = {0.0f, 0.0f, 0.0f, 1.0f};
-    m_d3dctxVLC->OMSetRenderTargets( 1, &read_write->m_textureRenderTarget, NULL );
-    m_d3dctxVLC->ClearRenderTargetView( read_write->m_textureRenderTarget, blackRGBA);
+    
+    m_d3dctxVLC->OMSetRenderTargets( 1, &read_write[write_index]->m_textureRenderTarget, NULL );
+    m_d3dctxVLC->ClearRenderTargetView( read_write[write_index]->m_textureRenderTarget, blackRGBA);
+
     return true;
 }
 
@@ -487,7 +498,8 @@ void* RenderAPI_D3D11::getVideoFrame(unsigned width, unsigned height, bool* out_
    
     *out_updated = m_updated;
     m_updated = false;
-    result = read_write->m_textureShaderInput;
+    size_t read_index = write_on_first ? 1 : 0;
+    result = read_write[read_index]->m_textureShaderInput;
 
     if(m_width != width || m_height != height)
     {
