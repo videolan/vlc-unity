7308d02fa5cb668303d317d9f8a3d00ed96bc774
Martin Finkel <me@martinfinkel.com>
2026-04-29 16:13:24 +0700

    unity: move VLCInput into LibVLCSharp namespace

--- Assets/VLCUnity/Demos/Scripts/VLCPlayerExampleCamera.cs
+++ Assets/VLCUnity/Demos/Scripts/VLCPlayerExampleCamera.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using LibVLCSharp;
 
 /// <summary>
 /// A simple free camera to be added to a Unity game object.
--- Assets/VLCUnity/Internal/VLCInput.cs
+++ Assets/VLCUnity/Internal/VLCInput.cs
@@ -3,152 +3,155 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 #endif
 
-/// <summary>
-/// A centralized wrapper to handle both Unity's New Input System and the Legacy Input Manager.
-/// </summary>
-public static class VLCInput
+namespace LibVLCSharp
 {
-    public static bool FastMode()
+    /// <summary>
+    /// A centralized wrapper to handle both Unity's New Input System and the Legacy Input Manager.
+    /// </summary>
+    public static class VLCInput
     {
+        public static bool FastMode()
+        {
 #if ENABLE_INPUT_SYSTEM
-        return Keyboard.current != null && (Keyboard.current.leftShiftKey.isPressed || Keyboard.current.rightShiftKey.isPressed);
+            return Keyboard.current != null && (Keyboard.current.leftShiftKey.isPressed || Keyboard.current.rightShiftKey.isPressed);
 #elif ENABLE_LEGACY_INPUT_MANAGER
-            return Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+                return Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
 #else
-            return false;
+                return false;
 #endif
-    }
+        }
 
-    public static bool MoveLeft()
-    {
+        public static bool MoveLeft()
+        {
 #if ENABLE_INPUT_SYSTEM
-        return Keyboard.current != null && (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed);
+            return Keyboard.current != null && (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed);
 #elif ENABLE_LEGACY_INPUT_MANAGER
-            return Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
+                return Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
 #else
-            return false;
+                return false;
 #endif
-    }
+        }
 
-    public static bool MoveRight()
-    {
+        public static bool MoveRight()
+        {
 #if ENABLE_INPUT_SYSTEM
-        return Keyboard.current != null && (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed);
+            return Keyboard.current != null && (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed);
 #elif ENABLE_LEGACY_INPUT_MANAGER
-            return Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
+                return Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
 #else
-            return false;
+                return false;
 #endif
-    }
+        }
 
-    public static bool MoveForward()
-    {
+        public static bool MoveForward()
+        {
 #if ENABLE_INPUT_SYSTEM
-        return Keyboard.current != null && (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed);
+            return Keyboard.current != null && (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed);
 #elif ENABLE_LEGACY_INPUT_MANAGER
-            return Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+                return Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
 #else
-            return false;
+                return false;
 #endif
-    }
+        }
 
-    public static bool MoveBack()
-    {
+        public static bool MoveBack()
+        {
 #if ENABLE_INPUT_SYSTEM
-        return Keyboard.current != null && (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed);
+            return Keyboard.current != null && (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed);
 #elif ENABLE_LEGACY_INPUT_MANAGER
-            return Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
+                return Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
 #else
-            return false;
+                return false;
 #endif
-    }
+        }
 
-    public static bool MoveLocalUp()
-    {
+        public static bool MoveLocalUp()
+        {
 #if ENABLE_INPUT_SYSTEM
-        return Keyboard.current != null && Keyboard.current.qKey.isPressed;
+            return Keyboard.current != null && Keyboard.current.qKey.isPressed;
 #elif ENABLE_LEGACY_INPUT_MANAGER
-            return Input.GetKey(KeyCode.Q);
+                return Input.GetKey(KeyCode.Q);
 #else
-            return false;
+                return false;
 #endif
-    }
+        }
 
-    public static bool MoveLocalDown()
-    {
+        public static bool MoveLocalDown()
+        {
 #if ENABLE_INPUT_SYSTEM
-        return Keyboard.current != null && Keyboard.current.eKey.isPressed;
+            return Keyboard.current != null && Keyboard.current.eKey.isPressed;
 #elif ENABLE_LEGACY_INPUT_MANAGER
-            return Input.GetKey(KeyCode.E);
+                return Input.GetKey(KeyCode.E);
 #else
-            return false;
+                return false;
 #endif
-    }
+        }
 
-    public static bool MoveWorldUp()
-    {
+        public static bool MoveWorldUp()
+        {
 #if ENABLE_INPUT_SYSTEM
-        return Keyboard.current != null && (Keyboard.current.rKey.isPressed || Keyboard.current.pageUpKey.isPressed);
+            return Keyboard.current != null && (Keyboard.current.rKey.isPressed || Keyboard.current.pageUpKey.isPressed);
 #elif ENABLE_LEGACY_INPUT_MANAGER
-            return Input.GetKey(KeyCode.R) || Input.GetKey(KeyCode.PageUp);
+                return Input.GetKey(KeyCode.R) || Input.GetKey(KeyCode.PageUp);
 #else
-            return false;
+                return false;
 #endif
-    }
+        }
 
-    public static bool MoveWorldDown()
-    {
+        public static bool MoveWorldDown()
+        {
 #if ENABLE_INPUT_SYSTEM
-        return Keyboard.current != null && (Keyboard.current.fKey.isPressed || Keyboard.current.pageDownKey.isPressed);
+            return Keyboard.current != null && (Keyboard.current.fKey.isPressed || Keyboard.current.pageDownKey.isPressed);
 #elif ENABLE_LEGACY_INPUT_MANAGER
-            return Input.GetKey(KeyCode.F) || Input.GetKey(KeyCode.PageDown);
+                return Input.GetKey(KeyCode.F) || Input.GetKey(KeyCode.PageDown);
 #else
-            return false;
+                return false;
 #endif
-    }
+        }
 
-    public static Vector2 MouseDelta()
-    {
+        public static Vector2 MouseDelta()
+        {
 #if ENABLE_INPUT_SYSTEM
-        if (Mouse.current == null) return Vector2.zero;
-        return new Vector2(Mouse.current.delta.x.ReadValue() * 0.1f, Mouse.current.delta.y.ReadValue() * 0.1f);
+            if (Mouse.current == null) return Vector2.zero;
+            return new Vector2(Mouse.current.delta.x.ReadValue() * 0.1f, Mouse.current.delta.y.ReadValue() * 0.1f);
 #elif ENABLE_LEGACY_INPUT_MANAGER
-            return new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+                return new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 #else
-            return Vector2.zero;
+                return Vector2.zero;
 #endif
-    }
+        }
 
-    public static float ScrollDelta()
-    {
+        public static float ScrollDelta()
+        {
 #if ENABLE_INPUT_SYSTEM
-        return Mouse.current != null ? Mouse.current.scroll.y.ReadValue() * 0.01f : 0f;
+            return Mouse.current != null ? Mouse.current.scroll.y.ReadValue() * 0.01f : 0f;
 #elif ENABLE_LEGACY_INPUT_MANAGER
-            return Input.GetAxis("Mouse ScrollWheel");
+                return Input.GetAxis("Mouse ScrollWheel");
 #else
-            return 0f;
+                return 0f;
 #endif
-    }
+        }
 
-    public static bool LookButtonDown()
-    {
+        public static bool LookButtonDown()
+        {
 #if ENABLE_INPUT_SYSTEM
-        return Mouse.current != null && Mouse.current.rightButton.wasPressedThisFrame;
+            return Mouse.current != null && Mouse.current.rightButton.wasPressedThisFrame;
 #elif ENABLE_LEGACY_INPUT_MANAGER
-            return Input.GetKeyDown(KeyCode.Mouse1);
+                return Input.GetKeyDown(KeyCode.Mouse1);
 #else
-            return false;
+                return false;
 #endif
-    }
+        }
 
-    public static bool LookButtonUp()
-    {
+        public static bool LookButtonUp()
+        {
 #if ENABLE_INPUT_SYSTEM
-        return Mouse.current != null && Mouse.current.rightButton.wasReleasedThisFrame;
+            return Mouse.current != null && Mouse.current.rightButton.wasReleasedThisFrame;
 #elif ENABLE_LEGACY_INPUT_MANAGER
-            return Input.GetKeyUp(KeyCode.Mouse1);
+                return Input.GetKeyUp(KeyCode.Mouse1);
 #else
-            return false;
+                return false;
 #endif
+        }
     }
 }
