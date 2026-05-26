aa03f57db9538215f9ec2036ff8f57ea129e6b4d
Krish Ganatra <ganatrakrish2882005@gmail.com>
2026-05-26 21:20:34 +0530

    scenes: update VLCSubtitles to use VLCMediaPlayer

--- Assets/VLCUnity/Demos/Scripts/VLCSubtitles.cs
+++ Assets/VLCUnity/Demos/Scripts/VLCSubtitles.cs
@@ -9,126 +9,46 @@ using LibVLCSharp;
 // In this particular sample, we discover how to get and set subtitles information.
 // For libvlcsharp usage documentation, please visit https://code.videolan.org/videolan/LibVLCSharp/-/blob/master/docs/home.md
 /// On Android, make sure you require Internet access in your manifest to be able to access internet-hosted videos in these demo scenes.
+
 public class VLCSubtitles : MonoBehaviour
 {
-    LibVLC _libVLC;
-    MediaPlayer _mediaPlayer;
-    const int seekTimeDelta = 5000;
-    Texture2D tex = null;
-    bool playing;
+    [SerializeField] private VLCMediaPlayer mediaPlayer;
 
-    void Awake()
+    void Start()
     {
-        Core.Initialize(Application.dataPath);
-
-        _libVLC = new LibVLC(enableDebugLogs: true);
-
-        Application.SetStackTraceLogType(LogType.Log, StackTraceLogType.None);
-        //_libVLC.Log += (s, e) => UnityEngine.Debug.Log(e.FormattedLog); // enable this for logs in the editor
-
-        PlayPause();
-    }
-
-    public void SeekForward()
-    {
-        Debug.Log("[VLC] Seeking forward !");
-        _mediaPlayer.SetTime(_mediaPlayer.Time + seekTimeDelta);
-    }
-
-    public void SeekBackward()
-    {
-        Debug.Log("[VLC] Seeking backward !");
-        _mediaPlayer.SetTime(_mediaPlayer.Time - seekTimeDelta);
-    }
-
-    void OnDisable() 
-    {
-        _mediaPlayer?.Stop();
-        _mediaPlayer?.Dispose();
-        _mediaPlayer = null;
-
-        _libVLC?.Dispose();
-        _libVLC = null;
-    }
-
-    public async void PlayPause()
-    {
-        Debug.Log ("[VLC] Toggling Play Pause !");
-        if (_mediaPlayer == null)
-        {
-            _mediaPlayer = new MediaPlayer(_libVLC);
-        }
-        if (_mediaPlayer.IsPlaying)
-        {
-            _mediaPlayer.Pause();
-        }
-        else
-        {
-            playing = true;
-
-            if(_mediaPlayer.Media == null)
-            {
-                // playing remote media
-                _mediaPlayer.Media = new Media(new Uri("https://github.com/Matroska-Org/matroska-test-files/blob/master/test_files/test5.mkv?raw=true"));
-            }
-
-            await _mediaPlayer.PlayAsync();
-
-            var trackList = _mediaPlayer.Tracks(TrackType.Text);
-
-            foreach (var track in trackList)
-            {
-                Debug.Log($"Language {track.Language}, id {track.Id}");
-            }
-
-            // we select the japanese track
-            _mediaPlayer.Select(trackList.Single(t => t.Language.Equals("jpn")));
-
-            // we can also use the track id to select it (11 is the ID of the japanese subtitle track)
-            // _mediaPlayer.Select(TrackType.Text, "spu/11");
-
-            // if you would like to add an external subtitle file (.srt, .ass, etc.), use the _mediaPlayer.AddSlave method instead
-            // https://code.videolan.org/videolan/LibVLCSharp/-/blob/3.x/docs/how_do_I_do_X.md#how-do-i-set-subtitles
-
-            // do not forget to dispose of the trackList when you are done with it
-            trackList.Dispose();
-        }
-    }
-
-    public void Stop ()
-    {
-        Debug.Log ("[VLC] Stopping Player !");
-
-        playing = false;
-        _mediaPlayer?.Stop();
-
-        // there is no need to dispose every time you stop, but you should do so when you're done using the mediaplayer and this is how:
-        // _mediaPlayer?.Dispose();
-        // _mediaPlayer = null;
-    }
-
-    void Update()
-    {
-        if(!playing)
-        {
-            if (tex != null && _mediaPlayer != null)
-                TextureHelper.UpdateTexture(tex, ref _mediaPlayer);
+        if (mediaPlayer == null || mediaPlayer.MediaPlayer == null)
             return;
+
+        mediaPlayer.OnPlayerStateChanged.AddListener(OnPlayerStateChanged);
+    }
+
+    private void OnPlayerStateChanged(VLCState state)
+    {
+        if (state != VLCState.Playing)
+            return;
+
+        var trackList = mediaPlayer.MediaPlayer.Tracks(TrackType.Text);
+
+        foreach (var track in trackList)
+        {
+            Debug.Log($"Language {track.Language}, id {track.Id}");
         }
 
-        if (tex == null)
-        {
-            // If received size is not null, it and scale the texture
-            tex = TextureHelper.CreateNativeTexture(ref _mediaPlayer, linear: true);
-            if (tex != null)
-            {
-                Debug.Log("Creating texture with height " + tex.height + " and width " + tex.width);
-                GetComponent<Renderer>().material.mainTexture = tex;
-            }
-        }
-        else
-        {
-            TextureHelper.UpdateTexture(tex, ref _mediaPlayer);
-        }
+        mediaPlayer.MediaPlayer.Select(trackList.Single(t => t.Language.Equals("jpn")));
+
+        // we can also use the track id to select it (11 is the ID of the japanese subtitle track)
+        // _mediaPlayer.Select(TrackType.Text, "spu/11");
+        
+        // if you would like to add an external subtitle file (.srt, .ass, etc.), use the _mediaPlayer.AddSlave method instead
+        // https://code.videolan.org/videolan/LibVLCSharp/-/blob/3.x/docs/how_do_I_do_X.md#how-do-i-set-subtitles
+
+        trackList.Dispose();
+
+        mediaPlayer.OnPlayerStateChanged.RemoveListener(OnPlayerStateChanged);
+    }
+
+    void OnDestroy()
+    {
+        mediaPlayer.OnPlayerStateChanged.RemoveListener(OnPlayerStateChanged);
     }
 }
